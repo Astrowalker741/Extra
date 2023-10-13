fc9f65d8d1d30182b9bfc089a95974913f287830
Astrowalker741 <kakasiboy741@gmail.com>
2023-10-13 11:01:28 +0100

    Astro

---
+++ menu.js
@@ -0,0 +1,9 @@
+const menuEl= document.getElementById("menu-icon");
+const menuli= document.getElementById("menu-list");
+
+menuEl.addEventListener("click", function(){
+    menuli.classList.toggle("hide")
+})
+
+
+
